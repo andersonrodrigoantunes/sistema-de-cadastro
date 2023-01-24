4511df0992392f43ac942ab3bd2e92a3cf9c46a7
andersonrodrigoantunes <ander.antunes.ar@gmail.com>
2023-01-24 15:58:51 -0300

    adicionando mais um console log para teste

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao bando de dados")
 // mais um console log de teste
 console.log("O Anderson fez esse commit para testar")
+console.log("Enviando para GitHub")
